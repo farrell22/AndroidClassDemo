1232ac557acb5bb5fdc6db44c397af38575156d7
Clementine Farrell <71611480+farrell22@users.noreply.github.com>
2023-02-23 19:25:33 -0500

    class 2-23 part 1

--- app/src/main/java/com/kroger/classdemoapp/MainActivity.kt
+++ app/src/main/java/com/kroger/classdemoapp/MainActivity.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.kroger.classdemoapp.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.character_card_view)
+        /*
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.fragment_container_view, CharacterListFragment())
+        }
+*/
+        /*
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(binding.fragmentContainerView, CharacterListFragment())
+            addToBackStack(null)
+        }
 
+         */
         val recyclerView = findViewById<RecyclerView>(R.id.character_recycler_view)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
